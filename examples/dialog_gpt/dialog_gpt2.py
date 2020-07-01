9b631d74c9675d16d2ec9294674497a3cb3a947d
wangfeng <wangfeng2@huya.com>
2020-07-01 16:15:56 +0800

    fix gpt2 model

--- examples/dialog_gpt/dialog_gpt2.py
+++ examples/dialog_gpt/dialog_gpt2.py
@@ -3,21 +3,56 @@ from torch import nn
 from transformers.modeling_gpt2 import GPT2Config, GPT2LMHeadModel
 
 
-def accuracy(output, target, topk=(1,)):
+def compute_accuracy(logits, labels, device, pad_id=-100):
+    """
+    计算非pad_id的平均loss和准确率
+    :param outputs:
+    :param labels:
+    :param device:
+    :return:
+    """
+
+    # 用前n-1个token，预测出第n个token
+    # 用第i个token的prediction_score用来预测第i+1个token。
+    # 假定有input有n个token，则shift_logits表示model中第[0,n-2]个token的prediction_score，shift_labels表示第[1，n-1]的label
+    shift_logits = logits[..., :-1, :].contiguous()
+    shift_labels = labels[..., 1:].contiguous().to(device)
+
+    _, preds = shift_logits.max(
+        dim=-1
+    )  # preds表示对应的prediction_score预测出的token在voca中的id。维度为[batch_size,token_len]
+
+    # 对非pad_id的token的loss进行求平均，且计算出预测的准确率
+    not_ignore = shift_labels.ne(
+        pad_id
+    )  # 进行非运算，返回一个tensor，若targets_view的第i个位置为pad_id，则置为0，否则为1
+    num_targets = not_ignore.long().sum().item()  # 计算target中的非pad_id的数量
+
+    correct = (shift_labels == preds) & not_ignore  # 计算model预测正确的token的个数，排除pad的tokne
+    correct = correct.float().sum()
+
+    accuracy = correct / num_targets
+    return accuracy
+
+
+def accuracy(logits, target, topk=(1,), pad_id=0):
     """Computes the accuracy over the k top predictions for the specified values of k"""
     with torch.no_grad():
-        maxk = max(topk)
-        batch_size = target.size(0)
+        _, preds = logits.max(
+            dim=-1
+        )  # preds表示对应的prediction_score预测出的token在voca中的id。维度为[batch_size,token_len]
 
-        _, pred = output.topk(maxk, 1, True, True)
-        pred = pred.t()
-        correct = pred.eq(target.view(1, -1).expand_as(pred))
+        not_ignore = target.ne(
+            pad_id
+        )  # 进行非运算，返回一个tensor，若targets_view的第i个位置为pad_id，则置为0，否则为1
+        num_targets = not_ignore.long().sum().item()  # 计算target中的非pad_id的数量
 
-        res = []
-        for k in topk:
-            correct_k = correct[:k].view(-1).float().sum(0, keepdim=True)
-            res.append(correct_k.mul_(100.0 / batch_size))
-        return res
+        correct = (target == preds) & not_ignore  # 计算model预测正确的token的个数，排除pad的tokne
+        correct = correct.float().sum()
+
+        accuracy = correct / num_targets
+
+        return accuracy
 
 
 class DialogGPT2(nn.Module):
@@ -25,6 +60,7 @@ class DialogGPT2(nn.Module):
         super().__init__()
         if pretrained_model:
             # resume from a pretrained model
+            print("resume from a pretrained model: %s" % pretrained_model)
             self.gpt2_model = GPT2LMHeadModel.from_pretrained(pretrained_model)
         else:
             # start from scratch
@@ -34,6 +70,8 @@ class DialogGPT2(nn.Module):
             # resize vocab size
             self.gpt2_model.resize_token_embeddings(vocab_size)
 
+        # self.smoothing = LabelSmoothing(local_rank, self.vocab.size, self.vocab.padding_idx, smoothing_factor)
+
     def forward(
         self,
         context_input_ids,
@@ -42,8 +80,22 @@ class DialogGPT2(nn.Module):
         lm_labels=None,
         **kwargs
     ):
+
+        # logits, *_ = self.gpt2_model(
+        #     context_input_ids,
+        #     token_type_ids=context_segment_ids,
+        #     attention_mask=context_masks,
+        # )
+
+        # predicts = {"logits": logits}
         loss = None
-        if lm_labels:
+        metrics = {}
+
+        # if lm_labels is not None:
+        #     loss, accuracy = calculate_loss_and_accuracy(logits, lm_labels, logits.device, pad_id=0)
+        #     metrics["accuracy"] = accuracy
+
+        if lm_labels is not None:
             # train mode
             loss, logits, *_ = self.gpt2_model(
                 context_input_ids,
@@ -51,6 +103,10 @@ class DialogGPT2(nn.Module):
                 token_type_ids=context_segment_ids,
                 attention_mask=context_masks,
             )
+            metrics["accuracy"] = compute_accuracy(
+                logits, lm_labels, logits.device, pad_id=-100
+            )
+
         else:
             logits, *_ = self.gpt2_model(
                 context_input_ids,
@@ -59,15 +115,15 @@ class DialogGPT2(nn.Module):
             )
 
         predicts = {"logits": logits}
-        metrics = {}
-
-        if lm_labels:
-            # measure accuracy and record loss
-            acc1, acc5 = accuracy(
-                logits.view(-1, logits.shape[-1]), lm_labels.view(-1), topk=(1, 5)
-            )
-            metrics["accuracy"] = acc1
-            metrics["acc1"] = acc1
-            metrics["acc5"] = acc5
 
         return predicts, loss, metrics
+
+    # def label_smotthing_loss(self, y_pred, y, y_mask, avg=True):
+    #     seq_len, bsz = y.size()
+
+    #     y_pred = torch.log(y_pred.clamp(min=1e-8))
+    #     loss = self.smoothing(y_pred.view(seq_len * bsz, -1), y.view(seq_len * bsz, -1))
+    #     if avg:
+    #         return loss / torch.sum(y_mask)
+    #     else:
+    #         return loss / bsz
